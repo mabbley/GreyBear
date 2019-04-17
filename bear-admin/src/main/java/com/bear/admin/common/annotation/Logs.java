e25fe6a6f8570977df636a7c7736d93c9e40c734
yoyo <mabnygluck@163.com>
2019-04-17 15:20:47 +0800

    项目初始化

---
+++ bear-admin/src/main/java/com/bear/admin/common/annotation/Logs.java
@@ -0,0 +1,7 @@
+package com.bear.admin.common.annotation;
+
+/**
+ * Created by mby on 2019/4/17.
+ */
+public @interface Logs {
+}
---
+++ bear-admin/src/main/java/com/bear/admin/common/annotation/aspect/LogsAspect.java
@@ -0,0 +1,14 @@
+package com.bear.admin.common.annotation.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by mby on 2019/4/17.
+ */
+@Slf4j
+@Aspect
+@Component
+public class LogsAspect {
+}
