3b01b38b9bcc7401cedc8f3568b0a52a33e9f829
yoyo <mabnygluck@163.com>
2019-04-23 09:42:18 +0800

    项目初始化

---
+++ bear-admin/src/main/java/com/bear/admin/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bear.admin.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by mby on 2019/4/22.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/main").setViewName("main");
+    }
+}
